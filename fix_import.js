b45bdf6f928bb991d110d2c5bbab0c5eb28d5adf
phquevedo <pquevedo2011.ph@gmail.com>
2026-06-26 09:52:26 -0300

    chore: remove temporary scripts

--- fix_import.js
+++
@@ -1,4 +0,0 @@
-﻿const fs = require('fs');
-let content = fs.readFileSync('src/app/(dashboard)/membros/MembersClient.tsx', 'utf8');
-content = content.replace("import Link from 'next/link'", "import Link from 'next/link'\nimport { toast } from 'sonner'");
-fs.writeFileSync('src/app/(dashboard)/membros/MembersClient.tsx', content, 'utf8');
