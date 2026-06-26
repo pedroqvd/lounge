5f8a575828bf08532d7df059af4bb66d06a59998
phquevedo <pquevedo2011.ph@gmail.com>
2026-06-26 08:40:18 -0300

    chore: clean up remaining temporary scripts

--- check_members.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-let task = fs.readFileSync('src/app/(dashboard)/membros/MembersClient.tsx', 'utf8');
-console.log("Length:", task.length);
--- extract_modal.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src/app/(dashboard)/membros/MembersClient.tsx', 'utf8');
-
-// Extract the Modal part
-const modalStart = content.indexOf('<Dialog.Root open={isModalOpen} onOpenChange={setIsModalOpen}>');
-const modalEnd = content.indexOf('</Dialog.Root>', modalStart) + 14;
-
-console.log(content.substring(modalStart, modalEnd));
