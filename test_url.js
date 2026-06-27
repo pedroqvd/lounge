3a6ce5d59547ff0adf6a56970adca48fd23f7712
phquevedo <pquevedo2011.ph@gmail.com>
2026-06-27 00:48:52 -0300

    chore: remove test script

--- test_url.js
+++
@@ -1,7 +0,0 @@
-const https = require('https');
-const url = 'https://unltgnznpvjfwiqioomh.supabase.co/storage/v1/object/public/video%20publico/bg-hero.mp4';
-https.request(url, { method: 'HEAD' }, (res) => {
-  console.log('Status:', res.statusCode);
-  console.log('Content-Type:', res.headers['content-type']);
-  console.log('Content-Length:', res.headers['content-length']);
-}).end();
